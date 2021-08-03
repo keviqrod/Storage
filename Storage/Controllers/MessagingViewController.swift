9b960f6f0def8ddda6d4369a6f6cf5b202e62a02
keviqrod <64922975+keviqrod@users.noreply.github.com>
2021-08-03 15:11:42 -0400

    First commit

---
+++ Storage/Controllers/MessagingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessagingViewController.swift
+//  Storage
+//
+//  Created by Student on 8/3/21.
+//
+
+import UIKit
+
+class MessagingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
